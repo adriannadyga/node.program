6ecd7d14d876db3b05acdc104dcae6f86b550a3b
adriannadyga <50697746+adriannadyga@users.noreply.github.com>
2019-08-10 19:24:30 +0200

    Add files via upload

---
+++ program1.js
@@ -0,0 +1,51 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+
+            case '/ver':
+                console.log('System version ' + process.versions.node);
+                break;
+            
+            case '/lang':
+                console.log('System language ' + process.env.LANG);
+                break;
+                
+            default: 
+            process.stdout.write('Wrong instruction!\n');
+        } 
+    }
+});
+
+/*process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/exit': 
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+
+            case '/ver':
+                console.log('System version ' + process.versions.node);
+
+            case '/lang':
+                console.log('System language ' + process.env.LANG);
+                break;
+                
+            default: 
+            process.stderr.write('Wrong instruction!\n');
+        }
+    }
+});*/
+
